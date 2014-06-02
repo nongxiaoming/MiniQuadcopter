57b787e1050b2fa5e275da7cebc77448e0f40d74
xiaonong <nongxiaoming@gmail.com>
2014-06-02 23:59:00 +0800

    MiniControl:修改ADC驱动注释.

--- Software/MiniControl/example/3.adc/Hardware/adc.c
+++ Software/MiniControl/example/3.adc/Hardware/adc.c
@@ -24,61 +24,57 @@ void ADC1_GPIO_Configuration(void)
 //ADC1  DMA 配置
 void ADC1_DMA_Init(void)
 {
-ADC_InitTypeDef ADC_InitStructure;
-DMA_InitTypeDef DMA_InitStructure;
- /* Enable DMA1 clock */
+ ADC_InitTypeDef ADC_InitStructure;
+ DMA_InitTypeDef DMA_InitStructure;
+   /* 使能DMA1时钟 */
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
-/* DMA1 channel1 configuration ----------------------------------------------*/
-  DMA_DeInit(DMA1_Channel1); //选择DMA的通道1
+  /* 复位DMA1 通道1的配置 */
+  DMA_DeInit(DMA1_Channel1); 
   //设定从ADC外设的数据寄存器（ADC1_DR_Address）转移到内存（ADCConcertedValue）
-  //每次传输大小16位，使用DMA循环传输模式
+  /* 每次传输大小16位，使用DMA循环传输模式 */
   DMA_InitStructure.DMA_PeripheralBaseAddr = ADC1_DR_Address;
   DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)ADCConvertedValue;//数据缓冲区的地址
-  //外设为数据源
+  /* 外设为数据源 */
   DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
-  //数据缓冲区，大小2半字
+  /* 数据缓冲区，大小为6 */
   DMA_InitStructure.DMA_BufferSize = 6;
-  // 外设地址固定
+  /* 外设地址固定 */ 
   DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-  //内存地址增加，多组adc时，使能，数据传输时，内存增加
+  /* 内存地址增加，多组adc时，使能，数据传输时，内存增加 */
   DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-  //半字
+  /* 数据大小为半字，16bit */
   DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
   DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
-  //DMA循环传输
+  /* DMA循环传输 */
   DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
-  //优先级高
+  /* 优先级高 */
   DMA_InitStructure.DMA_Priority = DMA_Priority_High;
-  //??
+  /* 禁止内存到内存模式 */
   DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-  //执行
+  /* 初始化DMA1 通道1 */
   DMA_Init(DMA1_Channel1, &DMA_InitStructure);
   
-  /* Enable DMA1 channel1 */
+  /* 使能DMA通道1 */
   DMA_Cmd(DMA1_Channel1, ENABLE);
   
-  /* ADC1 configuration ------------------------------------------------------*/
-  //ADC独立模式	 相对于双重模式
+  /* ADC独立模式	 相对于双重模式 */
   ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
-  //扫描模式用于多通道采集
+  /* 扫描模式用于多通道采集 */
   ADC_InitStructure.ADC_ScanConvMode = ENABLE;
-  //开启连续转换模式   当转换完本组（可能是一个）继续重新开始执行
-  //相对于单次模式：转换一次后就结束
+  /* 相对于单次模式：转换一次后就结束 */
   ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
-  //不使用外部触发转换
+  /* 不使用外部触发转换 */
   ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
-  //采集数据右对齐
+  /* 采集数据右对齐 */
   ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-  //转换组的通道数目
+  /* 转换组的通道数目 */
   ADC_InitStructure.ADC_NbrOfChannel =6;
-  //执行
+  /* 初始化配置ADC1 */
   ADC_Init(ADC1, &ADC_InitStructure);
   
-  //配置ADC时钟，为PCLK2的8分频，即9MHz
+  /* 配置ADC时钟，为PCLK2的8分频，即9MHz */
   RCC_ADCCLKConfig(RCC_PCLK2_Div8);
-  /* ADC1 regular channel11 configuration */ 
-  //配置ADC1的通道11为55.5个采样周期
-  //默认组，adc1 ，通道11，排序为1,55.5周期
+
   //ADC1,ch0,序号1,55.5.。。
   ADC_RegularChannelConfig(ADC1, ADC_Channel_0,1, ADC_SampleTime_239Cycles5);
   //ADC1,ch1,序号1,55.5.。。
@@ -93,33 +89,28 @@ DMA_InitTypeDef DMA_InitStructure;
   ADC_RegularChannelConfig(ADC1, ADC_Channel_16,6, ADC_SampleTime_239Cycles5);
 
 
-  //----------------------使能温度传感器----------------------------
+  /* 使能片内温度传感器 */
   ADC_TempSensorVrefintCmd(ENABLE);
 
-  /* Enable ADC1 DMA */
-  //使能ADC_DMA
+  /* 使能ADC_DMA */
   ADC_DMACmd(ADC1, ENABLE);
   
-  /* Enable ADC1 */
-  //使能ADC
+  /* 使能ADC */
   ADC_Cmd(ADC1, ENABLE);
 
-  /* Enable ADC1 reset calibration register */ 
-  //使能ADC1的复位校准寄存器  
+  /* 使能ADC1的复位校准寄存器 */
   ADC_ResetCalibration(ADC1);
-  /* Check the end of ADC1 reset calibration register */
-  //等待校准完成
+	
+  /* 等待校准完成 */
   while(ADC_GetResetCalibrationStatus(ADC1));
 
-  /* Start ADC1 calibration */
-  //使能ADC1的开始校准寄存器
+  /* 使能ADC1的开始校准寄存器 */
   ADC_StartCalibration(ADC1);
-  /* Check the end of ADC1 calibration */
-  //等待完成
+	
+  /* 等待完成 */
   while(ADC_GetCalibrationStatus(ADC1));
      
-  /* Start ADC1 Software Conversion */ 
-  //使用软件触发，由于没有采用外部触发
+  /* 使用软件触发，由于没有采用外部触发 */
   ADC_SoftwareStartConvCmd(ADC1, ENABLE);
 
 }
