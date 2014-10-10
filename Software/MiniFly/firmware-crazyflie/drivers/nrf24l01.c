c09b923604612fd8625176349a50c87218aa9fbf
xiaonong <nongxiaoming@gmail.com>
2014-10-10 23:26:10 +0800

    modify the spiSendByte function to NRF24L01_SendByte.

--- Software/MiniFly/firmware-crazyflie/drivers/nrf24l01.c
+++ Software/MiniFly/firmware-crazyflie/drivers/nrf24l01.c
@@ -92,7 +92,7 @@ static void (*interruptCb)(void) = RT_NULL;
 /***********************
  * SPI private methods *
  ***********************/
-static char spiSendByte(char byte)
+static char NRF24L01_SendByte(char byte)
 {
   /* Loop while DR register in not emplty */
   while (SPI_I2S_GetFlagStatus(RADIO_SPI, SPI_I2S_FLAG_TXE) == RESET);
@@ -109,7 +109,7 @@ static char spiSendByte(char byte)
 
 static char spiReceiveByte(void)
 {
-  return spiSendByte(DUMMY_BYTE);
+	return NRF24L01_SendByte(DUMMY_BYTE);
 }
 
 /****************************************************************
@@ -125,7 +125,7 @@ unsigned char nrfReadReg(unsigned char address, char *buffer, int len)
   RADIO_EN_CS();
 
   /* Send the read command with the address */
-  status = spiSendByte( CMD_R_REG | (address&0x1F) );
+  status = spi_send_byte(CMD_R_REG | (address & 0x1F));
   /* Read LEN bytes */
   for(i=0; i<len; i++)
     buffer[i]=spiReceiveByte();
@@ -144,7 +144,7 @@ unsigned char nrfWriteReg(unsigned char address, char *buffer, int len)
   RADIO_EN_CS();
 
   /* Send the write command with the address */
-  status = spiSendByte( CMD_W_REG | (address&0x1F) );
+  status = NRF24L01_SendByte(CMD_W_REG | (address & 0x1F));
   /* Write LEN bytes */
   for(i=0; i<len; i++)
     spiSendByte(buffer[i]);
@@ -175,7 +175,7 @@ unsigned char nrfNop(void)
   unsigned char status;
 
   RADIO_EN_CS();
-  status = spiSendByte(CMD_NOP);
+  status = NRF24L01_SendByte(CMD_NOP);
   RADIO_DIS_CS();
 
   return status;
@@ -186,7 +186,7 @@ unsigned char nrfFlushRx(void)
   unsigned char status;
 
   RADIO_EN_CS();
-  status = spiSendByte(CMD_FLUSH_RX);
+  status = NRF24L01_SendByte(CMD_FLUSH_RX);
   RADIO_DIS_CS();
 
   return status;
@@ -197,7 +197,7 @@ unsigned char nrfFlushTx(void)
   unsigned char status;
 
   RADIO_EN_CS();
-  status = spiSendByte(CMD_FLUSH_TX);
+  status = NRF24L01_SendByte(CMD_FLUSH_TX);
   RADIO_DIS_CS();
 
   return status;
@@ -221,7 +221,7 @@ unsigned char nrfActivate(void)
   unsigned char status;
   
   RADIO_EN_CS();
-  status = spiSendByte(CMD_ACTIVATE);
+  status = NRF24L01_SendByte(CMD_ACTIVATE);
   spiSendByte(ACTIVATE_DATA);
   RADIO_DIS_CS();
 
@@ -239,10 +239,10 @@ unsigned char nrfWriteAck(unsigned int pipe, char *buffer, int len)
   RADIO_EN_CS();
 
   /* Send the read command with the address */
-  status = spiSendByte(CMD_W_ACK_PAYLOAD(pipe));
+  status = NRF24L01_SendByte(CMD_W_ACK_PAYLOAD(pipe));
   /* Read LEN bytes */
   for(i=0; i<len; i++)
-    spiSendByte(buffer[i]);
+	  NRF24L01_SendByte(buffer[i]);
 
   RADIO_DIS_CS();
 
@@ -258,7 +258,7 @@ unsigned char nrfReadRX(char *buffer, int len)
   RADIO_EN_CS();
 
   /* Send the read command with the address */
-  status = spiSendByte(CMD_R_RX_PAYLOAD);
+  status = NRF24L01_SendByte(CMD_R_RX_PAYLOAD);
   /* Read LEN bytes */
   for(i=0; i<len; i++)
     buffer[i]=spiReceiveByte();
