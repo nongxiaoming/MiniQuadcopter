99a80e73e0c1e4d764d96371a0cf163c1aad0b58
xiaonong <nongxiaoming@gmail.com>
2015-01-05 01:56:51 +0800

    update drv_motors.

--- Software/MiniFly/Firmware/drivers/drv_motors.c
+++ Software/MiniFly/Firmware/drivers/drv_motors.c
@@ -2,81 +2,145 @@
 #include "board.h"
 #include "drv_motors.h"
 
+// HW defines
+#define MOTOR1_TIM_PERIF     RCC_APB1Periph_TIM2
+#define MOTOR1_TIM           TIM2
+#define MOTOR1_TIM_DBG       DBGMCU_TIM2_STOP
+//#define MOTOR1_TIM_REMAP     GPIO_PartialRemap_TIM2
+
+#define MOTOR2_TIM_PERIF     RCC_APB1Periph_TIM4
+#define MOTOR2_TIM           TIM4
+#define MOTOR2_TIM_DBG       DBGMCU_TIM4_STOP
+
+#define MOTOR3_TIM_PERIF     RCC_APB1Periph_TIM3
+#define MOTOR3_TIM           TIM3
+#define MOTOR3_TIM_DBG       DBGMCU_TIM3_STOP
+#define MOTOR3_TIM_REMAP     GPIO_FullRemap_TIM3
+
+#define MOTOR4_TIM_PERIF     RCC_APB1Periph_TIM3
+#define MOTOR4_TIM           TIM3
+#define MOTOR4_TIM_DBG       DBGMCU_TIM3_STOP
+#define MOTOR4_TIM_REMAP     GPIO_FullRemap_TIM3
+
+
+#define MOTOR1_GPIO_PERIF          RCC_APB2Periph_GPIOA
+#define MOTOR1_GPIO_PORT           GPIOA
+#define MOTOR1_GPIO_PIN            GPIO_Pin_2 // T2_CH3
+#define MOTOR2_GPIO_PERIF          RCC_APB2Periph_GPIOB
+#define MOTOR2_GPIO_PORT           GPIOB
+#define MOTOR2_GPIO_PIN            GPIO_Pin_9 // T4_CH4
+#define MOTOR3_GPIO_PERIF          RCC_APB2Periph_GPIOC
+#define MOTOR3_GPIO_PORT           GPIOC
+#define MOTOR3_GPIO_PIN            GPIO_Pin_9 // T3_CH4
+#define MOTOR4_GPIO_PERIF          RCC_APB2Periph_GPIOC
+#define MOTOR4_GPIO_PORT           GPIOC
+#define MOTOR4_GPIO_PIN            GPIO_Pin_8 // T3_CH3
+
 static rt_uint16_t pwm_value[MOTORS_NUM_MAX];
 
 static rt_err_t rt_motors_init(rt_device_t dev)
 {
 
-	 TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-	TIM_OCInitTypeDef  TIM_OCInitStructure;
-	GPIO_InitTypeDef GPIO_InitStructure;
-	uint16_t PrescalerValue = 0;
+	 //Init structures
+  GPIO_InitTypeDef GPIO_InitStructure;
+  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+  TIM_OCInitTypeDef  TIM_OCInitStructure;
+
+  //Enable gpio and the timer
+  RCC_APB2PeriphClockCmd( RCC_APB2Periph_AFIO 
+	                    | MOTOR1_GPIO_PERIF
+						| MOTOR2_GPIO_PERIF 
+						| MOTOR3_GPIO_PERIF 
+						| MOTOR4_GPIO_PERIF, ENABLE);
+
+  RCC_APB1PeriphClockCmd( MOTOR1_TIM_PERIF 
+	                    | MOTOR2_TIM_PERIF 
+	                    | MOTOR3_TIM_PERIF
+	                    | MOTOR4_TIM_PERIF, ENABLE);
+
+  GPIO_AFIODeInit();  
+  //Remap MOTO3 TIM
+  GPIO_PinRemapConfig(MOTOR3_TIM_REMAP, ENABLE);
+
+  //Remap MOTO4 TIM
+  GPIO_PinRemapConfig(MOTOR4_TIM_REMAP, ENABLE);
 	
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_8 | GPIO_Pin_11;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_TIM2);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_TIM2);
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource8, GPIO_AF_TIM1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource11,GPIO_AF_TIM1);
-	
-	/* Compute the prescaler value */
-  PrescalerValue = (uint16_t) ((SystemCoreClock /2) / 20000000) - 1;
-	/* Time base configuration */
-  TIM_TimeBaseStructure.TIM_Period = 2000;									//重装值
-  TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;			//分频系数
+	 // Configure the GPIO for the MOTO1 output
+  GPIO_InitStructure.GPIO_Pin = MOTOR1_GPIO_PIN;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(MOTOR1_GPIO_PORT, &GPIO_InitStructure);
+
+  // Configure the GPIO for the MOTO2 output
+  GPIO_InitStructure.GPIO_Pin = MOTOR2_GPIO_PIN;
+  GPIO_Init(MOTOR2_GPIO_PORT, &GPIO_InitStructure);
+
+  // Configure the GPIO for the MOTO3 output
+  GPIO_InitStructure.GPIO_Pin = MOTOR3_GPIO_PIN;
+  GPIO_Init(MOTOR3_GPIO_PORT, &GPIO_InitStructure);
+
+  // Configure the GPIO for the MOTO2 output
+  GPIO_InitStructure.GPIO_Pin = MOTOR4_GPIO_PIN;
+  GPIO_Init(MOTOR4_GPIO_PORT, &GPIO_InitStructure);
+
+  //Timer configuration
+  TIM_TimeBaseStructure.TIM_Period = MOTORS_PWM_PERIOD;
+  TIM_TimeBaseStructure.TIM_Prescaler = MOTORS_PWM_PRESCALE;
   TIM_TimeBaseStructure.TIM_ClockDivision = 0;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-	
-		/* Compute the prescaler value */
-  PrescalerValue = (uint16_t) (SystemCoreClock / 20000000) - 1;
-	/* Time base configuration */
-  TIM_TimeBaseStructure.TIM_Period = 2000;									//重装值
-  TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;						//分频系数
+  TIM_TimeBaseInit(MOTOR1_TIM, &TIM_TimeBaseStructure);
+
+  TIM_TimeBaseStructure.TIM_Period = MOTORS_PWM_PERIOD;
+  TIM_TimeBaseStructure.TIM_Prescaler = MOTORS_PWM_PRESCALE;
   TIM_TimeBaseStructure.TIM_ClockDivision = 0;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
-	TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+  TIM_TimeBaseInit(MOTOR2_TIM, &TIM_TimeBaseStructure);
 	
-	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	 TIM_TimeBaseStructure.TIM_Period = MOTORS_PWM_PERIOD;
+  TIM_TimeBaseStructure.TIM_Prescaler = MOTORS_PWM_PRESCALE;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(MOTOR3_TIM, &TIM_TimeBaseStructure);
+
+  TIM_TimeBaseStructure.TIM_Period = MOTORS_PWM_PERIOD;
+  TIM_TimeBaseStructure.TIM_Prescaler = MOTORS_PWM_PRESCALE;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(MOTOR4_TIM, &TIM_TimeBaseStructure);
+
+  //PWM channels configuration (All identical!)
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
   TIM_OCInitStructure.TIM_Pulse = 0;
-  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OCInitStructure.TIM_OCPolarity = MOTORS_POLARITY;
+
+  TIM_OC3Init(MOTOR1_TIM, &TIM_OCInitStructure);
+  TIM_OC3PreloadConfig(MOTOR1_TIM, TIM_OCPreload_Enable);
+
+  TIM_OC4Init(MOTOR2_TIM, &TIM_OCInitStructure);
+  TIM_OC4PreloadConfig(MOTOR2_TIM, TIM_OCPreload_Enable);
+
+  TIM_OC4Init(MOTOR3_TIM, &TIM_OCInitStructure);
+  TIM_OC4PreloadConfig(MOTOR3_TIM, TIM_OCPreload_Enable);
+
+  TIM_OC3Init(MOTOR4_TIM, &TIM_OCInitStructure);
+  TIM_OC3PreloadConfig(MOTOR4_TIM, TIM_OCPreload_Enable);
+
+  //Enable the timer
+  TIM_Cmd(MOTOR1_TIM, ENABLE);
+  TIM_Cmd(MOTOR2_TIM, ENABLE);
+  TIM_Cmd(MOTOR3_TIM, ENABLE);
+  TIM_Cmd(MOTOR4_TIM, ENABLE);
 	
-  TIM_OC1Init(TIM2, &TIM_OCInitStructure);
-  TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
-	
-	TIM_OC2Init(TIM2, &TIM_OCInitStructure);
-  TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable);
-	
-	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-  TIM_OCInitStructure.TIM_Pulse = 0;
-  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-	TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
-	
-	TIM_OC1Init(TIM1, &TIM_OCInitStructure);
-  TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
-	
-	TIM_OC4Init(TIM1, &TIM_OCInitStructure);
-  TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Enable);
-	
-	TIM_ARRPreloadConfig(TIM2, ENABLE);
-  TIM_Cmd(TIM2, ENABLE);
-	
-	TIM_CtrlPWMOutputs(TIM1, ENABLE);
-	TIM_ARRPreloadConfig(TIM1, ENABLE);
-  TIM_Cmd(TIM1, ENABLE);
+	TIM_ARRPreloadConfig(MOTOR1_TIM, ENABLE);
+  TIM_ARRPreloadConfig(MOTOR2_TIM, ENABLE);
+  TIM_ARRPreloadConfig(MOTOR3_TIM, ENABLE);
+  TIM_ARRPreloadConfig(MOTOR4_TIM, ENABLE);
+  // Halt timer during debug halt.
+  DBGMCU_Config(MOTOR1_TIM_DBG, ENABLE);
+  DBGMCU_Config(MOTOR2_TIM_DBG, ENABLE);
+  DBGMCU_Config(MOTOR3_TIM_DBG, ENABLE);
+  DBGMCU_Config(MOTOR4_TIM_DBG, ENABLE);
 	
   return RT_EOK;
 }
@@ -132,10 +196,10 @@ static rt_size_t rt_motors_write(rt_device_t dev, rt_off_t pos,
 			pwm_value[pos+index] = MOTORS_PWM_MAX;
 		  }
     }
-	TIM2->CCR1 = pwm_value[0] - 1000;
-	TIM2->CCR2 = pwm_value[1] - 1000;
-	TIM1->CCR1 = pwm_value[2] - 1000;
-	TIM1->CCR4 = pwm_value[3] - 1000;
+	MOTOR1_TIM->CCR3 = pwm_value[0] - MOTORS_PWM_MIN;
+	MOTOR2_TIM->CCR4 = pwm_value[1] - MOTORS_PWM_MIN;
+	MOTOR3_TIM->CCR4 = pwm_value[2] - MOTORS_PWM_MIN;
+	MOTOR4_TIM->CCR3 = pwm_value[3] - MOTORS_PWM_MIN;
 
     return size;
 }
