10864d2af1baa03efd015c19cd58be199407258d
xiaonong <nongxiaoming@gmail.com>
2015-01-04 23:09:48 +0800

    add the copter app.

---
+++ Software/MiniFly/Firmware/applications/application.cpp
@@ -0,0 +1,100 @@
+/*
+ * File      : application.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2009-01-05     Bernard      the first version
+ */
+
+/**
+ * @addtogroup STM32
+ */
+/*@{*/
+
+#include <board.h>
+#include <rtthread.h>
+#include <rtdevice.h>
+#include "drv_led.h"
+#include "spi_wifi_rw009.h"
+#include "copter.h"
+#include "drv_motors.h"
+#include "stm32_i2c.h"
+#include "mpu6050.h"
+
+#ifdef RT_USING_FINSH
+#include "shell.h"
+#endif
+
+#ifdef RT_USING_LWIP
+#include <lwip/sys.h>
+#include <lwip/api.h>
+#include <netif/ethernetif.h>
+#include "tcpserver.h"
+#include "telnet.h"
+#endif
+
+extern "C" void set_if(char* netif_name, char* ip_addr, char* gw_addr, char* nm_addr);
+extern "C" int lwip_system_init(void);
+
+void rt_init_thread_entry(void* parameter)
+{
+	rt_i2c_core_init();
+	rt_hw_i2c_init();
+	rt_hw_spi_init();
+	//³õÊ¼»¯Ä£ÄâI2C
+	//I2C_Soft_Init();
+	rt_hw_mpu6050_init("i2c1", MPU6050_DEFAULT_ADDRESS);
+	rt_motors_hw_init();
+/* LwIP Initialization */
+ #ifdef RT_USING_LWIP
+ 	{
+ 		extern void lwip_sys_init(void);
+
+ 		/* register ethernetif device */
+ 		eth_system_device_init();
+
+ 		/* initialize wifi interface */
+ 		rt_hw_wifi_init("spi10");
+
+ 		/* init lwip system */
+ 		lwip_system_init();
+ 		rt_kprintf("TCP/IP initialized!\n");
+ 		
+ 		set_if("w0","192.168.3.9","192.168.3.1","255.255.255.0");
+ 		rw009_join("rtthread_11n","rtthread_finsh");
+ 	}
+ #endif
+	rt_tcpserver_init();
+	apps_copter_init();
+		
+	
+ #ifdef RT_USING_FINSH
+ 	/* init finsh */
+ 	finsh_system_init();
+ 	finsh_set_device(RT_CONSOLE_DEVICE_NAME);
+ #endif
+  telnet_server_init();
+	sys_led_init();
+}
+
+int rt_application_init()
+{
+	rt_thread_t tid;
+
+	tid = rt_thread_create("init",
+								rt_init_thread_entry, RT_NULL,
+								2048, RT_THREAD_PRIORITY_MAX/3, 20);
+
+	if (tid != RT_NULL)
+		rt_thread_startup(tid);
+
+	return 0;
+}
+
+/*@}*/
---
+++ Software/MiniFly/Firmware/applications/copter.cpp
@@ -0,0 +1,100 @@
+/******************** (C) COPYRIGHT 2014 ANO Tech ***************************
+ * 作者		 ：匿名科创
+ * 文件名  ：AnoCopter.cpp
+ * 描述    ：匿名微型六轴飞行器
+ * 代码版本：V1.0
+ * 时间		 ：2014/10/13 
+ * 官网    ：www.anotc.com
+ * 淘宝    ：anotc.taobao.com
+ * 技术Q群 ：190169595
+**********************************************************************************/
+#include <rtthread.h>
+#include <rtdevice.h>
+#include "config.h"
+#include "params.h"
+#include "pidctrl.h"
+#include "commander.h"
+
+
+static void attitude_thread_entry(void* parameter)
+{	
+	while(1){
+	//更新传感器数据	
+	IMU_UpdateSensor();		
+	
+	//计算飞行器姿态
+	IMU_GetAttitude();
+	
+	//飞行器姿态内环控制
+	fc.Attitude_Inner_Loop();	
+	
+  rt_thread_delay(2);	
+	}
+}
+
+static void data_transmit_thread_entry(void* parameter)
+{
+	while(1){
+	//发送飞行器数据
+	cmd.Data_Exchange();
+	rt_thread_delay(10);
+	}	
+}
+
+static void receiver_thread_entry(void* parameter)
+{
+	
+	while(1){
+		
+  //遥控通道数据处理
+	rc.Cal_Command();
+	
+	//摇杆位置检查
+	rc.check_sticks();
+	
+	//失控保护检查
+	cmd.Failsafe_Check();
+	
+	//LED指示灯控制
+	config.Pilot_Light();
+	
+	rt_thread_delay(20);	
+	}
+}
+
+int apps_copter_init(void)
+{
+  rt_thread_t attitude_thread,data_transmit_thread,receiver_thread;
+	
+	//初始化IMU（惯性测量单元）
+	IMU_Init();	
+	
+	//初始化飞行控制
+	fc.Init();
+	
+	//初始化参数
+	Params_Init();
+	
+	//初始化Transmiter
+   cmd.Init("tcpserv");
+	
+	attitude_thread = rt_thread_create("attitude",attitude_thread_entry,RT_NULL,1024,12,5);
+	if(attitude_thread != RT_NULL)
+	{
+		rt_thread_startup(attitude_thread);
+	}
+	data_transmit_thread = rt_thread_create("transmit",data_transmit_thread_entry,RT_NULL,1024,13,5);
+	if(data_transmit_thread != RT_NULL)
+	{
+		rt_thread_startup(data_transmit_thread);
+	}
+		receiver_thread = rt_thread_create("receiver",receiver_thread_entry,RT_NULL,1024,14,5);
+	if(receiver_thread != RT_NULL)
+	{
+		rt_thread_startup(receiver_thread);
+	}
+	return 0;
+}
+
+
+/******************* (C) COPYRIGHT 2014 ANO TECH *****END OF FILE************/
