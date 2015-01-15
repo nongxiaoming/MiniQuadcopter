19bd0d34ead630d03b26b8faa449055aa4f99172
nongxiaoming <nongxiaoming@gmail.com>
2015-01-15 15:46:35 +0800

    fix the spi driver.

--- Software/MiniControl/Firmware/drivers/drv_spi.h
+++ Software/MiniControl/Firmware/drivers/drv_spi.h
@@ -1,42 +1,27 @@
-/*
- * File      : stm32f20x_40x_spi.h
- * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2009 RT-Thread Develop Team
- *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rt-thread.org/license/LICENSE
- *
- * Change Logs:
- * Date           Author       Notes
- * 20012-01-01    aozima       first implementation.
- */
+#ifndef __DRV_SPI_H
+#define __DRV_SPI_H
 
-#ifndef STM32F103_SPI_H_INCLUDED
-#define STM32F103_SPI_H_INCLUDED
-
-#include <stdint.h>
-#include <rtthread.h>
-
+#include <rtdevice.h>
 
 #include "stm32f10x.h"
+#include "stm32f10x_spi.h"
 
+#include "board.h"
+
+#define SPI_USE_DMA
 
 struct stm32_spi_bus
 {
+    struct rt_spi_bus parent;
     SPI_TypeDef * SPI;
 #ifdef SPI_USE_DMA
-    DMA_Stream_TypeDef * DMA_Stream_TX;
-    uint32_t DMA_Channel_TX;
-
-    DMA_Stream_TypeDef * DMA_Stream_RX;
-    uint32_t DMA_Channel_RX;
-
+    DMA_Channel_TypeDef * DMA_Channel_TX;
+    DMA_Channel_TypeDef * DMA_Channel_RX;
     uint32_t DMA_Channel_TX_FLAG_TC;
-//    uint32_t DMA_Channel_TX_FLAG_TE;
+    uint32_t DMA_Channel_TX_FLAG_TE;
     uint32_t DMA_Channel_RX_FLAG_TC;
-//    uint32_t DMA_Channel_RX_FLAG_TE;
-#endif /* #ifdef SPI_USE_DMA */
+    uint32_t DMA_Channel_RX_FLAG_TE;
+#endif /* SPI_USE_DMA */
 };
 
 struct stm32_spi_cs
@@ -45,5 +30,11 @@ struct stm32_spi_cs
     uint16_t GPIO_Pin;
 };
 
- void rt_hw_spi_init(void);
-#endif // STM32F20X_40X_SPI_H_INCLUDED
+/* public function list */
+rt_err_t stm32_spi_register(SPI_TypeDef * SPI,
+                            struct stm32_spi_bus * stm32_spi,
+                            const char * spi_bus_name);
+														
+int rt_hw_spi_init(void);
+														
+#endif // __DRV_SPI_H
